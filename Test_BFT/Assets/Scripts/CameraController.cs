e682fdf2cdefc77659cf6d0afb0b44e4e4f7f581
LP <yio.rango@gmail.com>
2021-03-22 22:17:27 +0100

    newcam

--- Test_BFT/Assets/Scripts/CameraController.cs
+++ Test_BFT/Assets/Scripts/CameraController.cs
@@ -4,43 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public GameObject Player;
-    public Transform TargetCamera;
-    //public GameObject CameraFolder;
-    public Transform CameLocation;
-    //public int locationIndicator = 2;
+    public Transform Car;
 
-    public Player_Movement controllerRef;
 
-    [Range(0, 1)] public float smoothTime = .5f;
-
-    private Rigidbody playerRb;
-
-    void Start()
+    void LateUpdate()
     {
-        //Player = GameObject.FindGameObjectWithTag("Player").transform;
-        //TargetPlayer = GameObject.FindGameObjectWithTag.;
-        //CameraFolder = Camera.main.gameObject;
-
-        controllerRef = Player.GetComponent<Player_Movement>();
-        playerRb = Player.GetComponent<Rigidbody>();
+        
     }
 
-
-    void FixedUpdate()
-    {
-        //if (Input.GetKeyDown(KeyCode.Tab))
-        //{
-        //    if (locationIndicator >= 4 || locationIndicator < 2) 
-        //        locationIndicator = 2;
-        //    else locationIndicator++;
-        //}
-
-        transform.position = CameLocation.position * (1 - smoothTime) + transform.position * smoothTime;
-        transform.LookAt(TargetCamera);
-
-        //Vector3 KPH = playerRb.velocity;
-
-        smoothTime = (playerRb.velocity.x >= 2) ? Mathf.Abs((playerRb.velocity.x) / 9) - 0.85f : -0.60f;
-    }
 }
