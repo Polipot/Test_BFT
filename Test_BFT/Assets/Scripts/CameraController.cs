10415b3b0a60a42003d9a0a08d53cdd10795ef82
LP <yio.rango@gmail.com>
2021-03-23 00:03:17 +0100

    Camera pas attacher

--- Test_BFT/Assets/Scripts/CameraController.cs
+++ Test_BFT/Assets/Scripts/CameraController.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform Car;
+    public Transform Target;
+    public float PositionSmoothTime;
+    public float RotationSmoothTime;
 
+    Transform pivot;
+    Camera cam;
 
-    void LateUpdate()
+    private void Start()
     {
-        
+        pivot = transform.GetChild(0).transform;
+        if (pivot == null)
+            Debug.LogError("Pas de pivot correct dans ce gameObject");
+
+        cam = pivot.GetChild(0).GetComponent<Camera>();
+        if (cam == null)
+            Debug.LogError("Pas decomponent Camera correct dans ce gameObject");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 positionSmoothDampvelocity = Vector3.zero;
+        this.transform.position = Vector3.SmoothDamp(this.transform.position, Target.position, ref positionSmoothDampvelocity, PositionSmoothTime);
+
+        float rotationSmoothDampVelocity = 0f;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, Mathf.SmoothDamp(this.transform.rotation.eulerAngles.y, target.rotation.eulerAngles.y, ref rotationSmoothDampVelocity, RotationSmoothTime), 0f));
     }
 
 }
